72a5e42a4325ec83ad5905f92807db2949db72b8
Alexandros Konstantakis <alexkonstadakis@gmail.com>
2024-12-09 20:28:58 +0200

    updated libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: akonstan <akonstan@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 22:16:51 by akonstan          #+#    #+#             */
-/*   Updated: 2024/12/08 22:54:25 by akonstan         ###   ########.fr       */
+/*   Updated: 2024/12/09 19:52:50 by akonstan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
-
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
