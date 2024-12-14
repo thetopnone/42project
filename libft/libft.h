23c8905d519abb53f4032e9a8e481eebf4c3a0bd
Alexandros Konstantakis <alexkonstadakis@gmail.com>
2024-12-14 23:32:33 +0200

    Added ft_strjoin. Updated libft.h with all the remaining functions. Norm checked

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: akonstan <akonstan@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 22:16:51 by akonstan          #+#    #+#             */
-/*   Updated: 2024/12/14 22:03:33 by akonstan         ###   ########.fr       */
+/*   Updated: 2024/12/14 23:30:59 by akonstan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,15 @@ int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
