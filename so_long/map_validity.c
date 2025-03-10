90db18b31286537a1ea06857cc5d46ba943d6afd
Alexandros Anargyros Konstantakis <akonstan@c1r11s4.42warsaw.pl>
2025-03-10 15:33:13 +0100

    added subjects and updated so_long

--- so_long/map_validity.c
+++ so_long/map_validity.c
@@ -6,7 +6,7 @@
 /*   By: akonstan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:16:39 by akonstan          #+#    #+#             */
-/*   Updated: 2025/03/07 17:07:47 by akonstan         ###   ########.fr       */
+/*   Updated: 2025/03/10 15:16:48 by akonstan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ int	ft_check_walls(t_data *data)
 	y = 0;
 	while (y < data->map->columns)
 	{
-		if (data->map->map_arr[y][0] != '1' ||
-		data->map->map_arr[y][data->map->rows - 1] != '1')
+		if (data->map->map_arr[0][y] != '1' ||
+		data->map->map_arr[data->map->rows - 1][y] != '1')
 			return (data->error->walls_error = 1);
 		y++;
 	}
 	while (x < data->map->rows)
 	{
-		if (data->map->map_arr[0][x] != '1' ||
-		data->map->map_arr[data->map->columns - 1][x] != '1')
+		if (data->map->map_arr[x][0] != '1' ||
+		data->map->map_arr[x][data->map->columns - 1] != '1')
 			return (data->error->walls_error = 1);
 		x++;
 	}
@@ -47,11 +47,11 @@ int	ft_map_objects(t_data *data)
 		y = 0;
 		while (data->map->map_arr[x][y] != '\0')
 		{
-			if (data->map->map_arr[y][x] == 'P')
+			if (data->map->map_arr[x][y] == 'P')
 				data->map->starts += 1;
-			if (data->map->map_arr[y][x] == 'E')
+			if (data->map->map_arr[x][y] == 'E')
 				data->map->exits += 1;
-			if (data->map->map_arr[y][x] == 'C')
+			if (data->map->map_arr[x][y] == 'C')
 				data->map->clcts += 1;
 			y++;
 		}
@@ -65,15 +65,15 @@ int	ft_map_objects(t_data *data)
 
 int	ft_valid_point(t_data *data, t_point point)
 {
-	if (point.x < 0 || point.y < 0 || point.x >= data->map->columns
-		|| point.y >= data->map->rows)
+	if (point.x < 0 || point.y < 0 || point.x >= data->map->rows
+		|| point.y >= data->map->columns)
 		return (1);
-	if (data->map->map_arr[point.y][point.x] == '1')
+	if (data->map->map_arr[point.x][point.y] == '1')
 		return (1);
 	return (0);
 }
 
-void	ft_update_queue(t_data data, t_point old, int	back)
+void	ft_update_queue(t_data data, t_point old, int	*back)
 {
 	t_point	next_pos;
 	int		j;
@@ -82,14 +82,21 @@ void	ft_update_queue(t_data data, t_point old, int	back)
 	while (j < 4)
 	{
 		if (j < 2)
-			next_pos.x = old.x + pow(-1, j);
-		else
-			next_pos.y = old.y + pow(-1, j);
-		if (ft_valid_point(data, next_pos) == 0
-			&& data->visited[next_pos.y][next_pos.x] == 0)
 		{
-			data->map->queue[back++] = next_pos;
-			data->visited[next_pos.y][next_pos.x] = 1;
+			next_pos.x = old.x + (int)pow(-1, j);
+			next_pos.y = old.y;
+		}
+		else
+		{
+			next_pos.y = old.y + (int)pow(-1, j);
+			next_pos.x = old.x;
+		}
+		if (ft_valid_point(data, next_pos) == 0
+			&& data->visited[next_pos.x][next_pos.y] == 0)
+		{
+			data->map->queue[*back] = next_pos;
+			(*back)++;
+			data->visited[next_pos.x][next_pos.y] = 1;
 		}
 		j++;
 	}
@@ -106,16 +113,17 @@ int	ft_check_valid_path(t_data *data)
 	data->map->queue = ft_calloc(data->map->rows * data->map->columns,
 			sizeof(t_point));
 	data->map->queue[back++] = data->map->start_pos;
-	data->visited[data->map->start_pos.y][data->map->start_pos.x] = 1;
+	data->visited[data->map->start_pos.x][data->map->start_pos.y] = 1;
 	while (front < back)
 	{
 		cur_pos = data->map->queue[front++];
 		if (ft_pos_tr(data, cur_pos) == 'E'
 			&& data->map->clctd == data->map->clcts)
 		{
+			free(data->map->queue);
 			return (1);
 		}
-		ft_update_queue(data, cur_pos, back);
+		ft_update_queue(data, cur_pos, &back);
 	}
 	return (0);
 }
