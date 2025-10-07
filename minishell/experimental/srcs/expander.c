4ca1e34a289df80b3bcde2eb543df26fd08b92f7
Alexandros Anargyros Konstantakis <akonstan@c3r11s1.42warsaw.pl>
2025-10-07 17:25:52 +0200

    EXpander notes

--- minishell/experimental/srcs/expander.c
+++ minishell/experimental/srcs/expander.c
@@ -12,8 +12,16 @@
 
 #include "../minishell.h"
 
+int	ft_expand_string
 //This function will get inside the pipeline and expand on all the variables 
 //that need to be expanded
-int	ft_expander(t_pipe *pipeline)
-{}
+//It only needs to handle $word , $? ,${word}line 
+int	ft_expander(t_pipe *pipeline, t_error *err)
+{
+	t_token		*cmd_chain;
+	t_redirect	*red_chain;
 
+	if (!pipeline)
+		return (err->expander = 1);
+	cmd_chain = pipeline
+}
